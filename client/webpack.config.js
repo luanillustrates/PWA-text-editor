98ab126a30891faf933f657361e3a745e2119889
luanillustrates <luanillustrates@gmail.com>
2023-10-22 11:56:59 +1030

    added logic, asset caching, loaders, scripts

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
